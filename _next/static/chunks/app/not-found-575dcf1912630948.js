75b466291f92c4d7ae492d937d1f9a6b9cb9dc51
Verumpraxis <info@verumpraxis.kz>
2026-09-01 19:06:03 +0500

    fix: add not-found fallback to render ClientApp on preview proxy URLs

---
+++ _next/static/chunks/app/not-found-575dcf1912630948.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{},function(n){n.O(0,[147,27,305,971,117,744],function(){return n(n.s=8305)}),_N_E=n.O()}]);
---
+++ nextjs_space/app/not-found.tsx
@@ -0,0 +1,5 @@
+import ClientApp from './_components/client-app';
+
+export default function NotFound() {
+  return <ClientApp />;
+}
